6884d030ba133b527f81059ae85a70760e9d27c9
wdelacruzl <wdelacruzl98@gmail.com>
2022-04-18 14:51:48 -0400

    2nd try... getting familiarized

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("My name is Wendy De la Cruz Lorenzo");
+        System.out.println("My name is Wendy De la Cruz Lorenzo. I want to be a good developer");
     }
 }
